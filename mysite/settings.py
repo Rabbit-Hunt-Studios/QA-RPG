e4296bab86a009902da878317fc5f91ae99454e5
RyukungG <ryu.nosuke@hotmail.com>
2022-10-14 14:25:48 +0700

    Add models and demo user

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa_rpg.apps.QaRpgConfig',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +46,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+AUTHENTICATION_BACKENDS = [
+    # username/password authentication
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -69,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -79,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -99,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -110,7 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- qa_rpg/models.py
+++ qa_rpg/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    damage = models.IntegerField(default=1)
+    currency = models.IntegerField(default=1)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def report(self):
+        return Report.objects.filter(question=self).count()
+
+    def commend(self):
+        return Commend.objects.filter(question=self).count()
+
+    def __str__(self):
+        """Return Question string."""
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    correct_answer = models.BooleanField()
+
+    def __str__(self):
+        """Return Choice string."""
+        return self.choice_text
+
+
+class Report(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Commend(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Player(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    max_hp = models.IntegerField(default=5)
+    current_hp = models.IntegerField(default=5)
+    currency = models.IntegerField(default=0)
+
+    def reset_hp(self):
+        self.current_hp = self.max_hp
+
+    def player_name(self):
+        return self.user.first_name
+
+
+class Log(models.Model):
+    log_text = models.CharField(max_length=300, default="")
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+    def clear_log(self):
+        self.log_text = ""
+
+    def add_log(self, text):
+        list_log = self.log_text.split(';')
+        if len(list_log)-1 >= 10:
+            del(list_log[0])
+            self.log_text = ";".join(list_log)
+        self.log_text += f"{text};"
+
+    def __str__(self):
+        """Return Question string."""
+        return self.log_text
+
