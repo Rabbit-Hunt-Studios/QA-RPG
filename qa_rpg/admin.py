98727b8adb95c392afca1289487167632f7b65a2
RyukungG <ryu.nosuke@hotmail.com>
2022-10-20 10:59:57 +0700

    fixed detail mistake

--- qa_rpg/admin.py
+++ qa_rpg/admin.py
@@ -9,7 +9,7 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['damage', 'currency'],
+        ('Question detail', {'fields': ['damage', 'currency'],
                               }),
     ]
     inlines = [ChoiceInline]
