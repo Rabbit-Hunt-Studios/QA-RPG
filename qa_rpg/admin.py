e056755facd59b20f1d997dd131151d4f97ae56a
RyukungG <ryu.nosuke@hotmail.com>
2022-10-20 10:40:12 +0700

    add admin page and superuser

--- qa_rpg/admin.py
+++ qa_rpg/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Question, Choice, Player
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['damage', 'currency'],
+                              }),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'damage', 'currency',
+                    'owner', 'enable', 'report', 'commend', 'correct_choice')
+    list_filter = ['owner', 'enable', 'damage', 'currency', 'report', 'commend']
+    search_fields = ['question_text', 'owner']
+
+
+admin.site.register(Question, QuestionAdmin)
