b8eaafd4c4bca32bcc93e1ad944d34b36c62ff44
Punn Chunwimaleung <khunpunn2546@gmail.com>
2022-10-23 11:13:14 +0700

    add dialogue for failed escape

--- qa_rpg/dialogue.py
+++ qa_rpg/dialogue.py
@@ -11,11 +11,14 @@ MONSTER = ["Elite Goblin",
            "Frenzied Werewolf",
            "Crystallised Skeleton"]
 WIN_DIALOGUE = ["The monster drew its last breath.",
-                "You stand victoriously on top of monster's corpse.",
+                "You stand victoriously on top of the monster's corpse.",
                 "You slashed through the monster's torso."]
 LOSE_DIALOGUE = ["The monster completely overpowered you.",
                  "You get caught the monster's rampage.",
                  "The monster smacks you across the room."]
+RUN_FAIL_DIALOGUE = ["The monster stands tall in front of your escape route.",
+                     "You try to run away but it utterly fails.",
+                     "The monster attacks and diminishes your hope of escaping."]
 RUN_DIALOGUE = ["You cower in the face of the monster.",
                 "The monster's sheer strength scared you away.",
                 "You fear the monster's menacing gazes."]
