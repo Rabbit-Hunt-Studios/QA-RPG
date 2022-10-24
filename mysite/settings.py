b17b0dd54a1396cb8d9878f5c84098169412a7c4
Mojizsz <isaraa.p@ku.th>
2022-10-24 20:25:00 +0700

    Added a dungeon pages template.

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'qa_rpg.apps.QaRpgConfig',
+    'tailwind',  
+    'django_browser_reload', 
+    'Dungeon', 
 ]
 
 MIDDLEWARE = [
@@ -46,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware", 
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -121,3 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'Dungeon'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
