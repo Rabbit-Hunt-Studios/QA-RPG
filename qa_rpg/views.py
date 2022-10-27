f8af33d4c1a1ef0e19726f98248352c0a792f712
Punn Chunwimaleung <khunpunn2546@gmail.com>
2022-10-27 23:24:07 +0700

    implement fees for creating questions

--- qa_rpg/views.py
+++ qa_rpg/views.py
@@ -1,13 +1,16 @@
 from django.views import generic
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.db.models import Q
 import random
 import difflib
 from .models import Question, Choice, Player, Log
 from .dialogue import Dialogue
 
-TREASURE_AMOUNT = [1, 5, 10, 15, 30]
+TREASURE_AMOUNT = [5, 10, 15, 30, 35]
 TREASURE_THRESHOLD = 0.55
+CATEGORY = ['General Knowledge', 'Entertainment', 'Science', 'Math',
+            'History', 'Technology', 'Sport']
 
 
 def get_player_log(player: Player):
@@ -32,7 +35,7 @@ class IndexView(generic.TemplateView):
     template_name = 'qa_rpg/index.html'
 
     def get(self, request):
-        player = Player.objects.get(pk=1)  # dummy player
+        player = Player.objects.get(user=request.user)
         log = get_player_log(player)
 
         check_url = check_player_activity(player, ["summon", "index"])
@@ -50,7 +53,7 @@ class DungeonView(generic.ListView):
     template_name = "qa_rpg/dungeon.html"
 
     def get(self, request):
-        player = Player.objects.get(pk=1)  # dummy player
+        player = Player.objects.get(user=request.user)
 
         check_url = check_player_activity(player, ["index", "dungeon"])
         if check_url is not None:
@@ -63,7 +66,7 @@ class DungeonView(generic.ListView):
 
 def action(request):
 
-    player = Player.objects.get(pk=1)  # dummy player
+    player = Player.objects.get(user=request.user)
     log = get_player_log(player)
     event = random.random()
 
@@ -92,7 +95,7 @@ class BattleView(generic.DetailView):
     template_name = 'battle.html'
 
     def get(self, request):
-        player = Player.objects.get(pk=1)  # dummy player
+        player = Player.objects.get(user=request.user)
 
         check_url = check_player_activity(player, ["battle", "found monster"])
         if check_url is not None:
@@ -101,7 +104,8 @@ class BattleView(generic.DetailView):
         if difflib.get_close_matches(player.activity, ["battle"]):
             question_id = int(player.activity[6:])
         else:
-            question_id = random.choice(Question.objects.all().values_list("id", flat=True))
+            question_id = random.choice(Question.objects.filter(~Q(owner=request.user))
+                                        .values_list("id", flat=True))
         question = Question.objects.get(pk=question_id)
 
         player.set_activity(f"battle{question_id}")
@@ -111,7 +115,7 @@ class BattleView(generic.DetailView):
 def check(request, question_id):
 
     question = Question.objects.get(pk=question_id)
-    player = Player.objects.get(pk=1)  # dummy player
+    player = Player.objects.get(user=request.user)
     log = get_player_log(player)
 
     try:
@@ -139,12 +143,14 @@ def check(request, question_id):
 
         if check_choice.correct_answer:
             log.add_log(Dialogue.WIN_DIALOGUE.get_text)
-            log.add_log(f"You earn {question.currency} coins.")
-            player.update_player_stats(dungeon_currency=question.currency, luck=0.03)
+            earn_coins = get_coins(question.damage)
+            log.add_log(f"You earn {earn_coins} coins.")
+            player.update_player_stats(dungeon_currency=earn_coins, luck=0.03)
             player.set_activity("dungeon")
         else:
             log.add_log(Dialogue.LOSE_DIALOGUE.get_text)
             player.update_player_stats(health=-question.damage)
+            question.add_coin()
             if player.check_death():
                 messages.error(request, "You lost consciousness in the dungeons.")
                 return render(request, "qa_rpg/index.html", {'player': player})
@@ -159,45 +165,61 @@ def check(request, question_id):
         return redirect("qa_rpg:battle")
 
 
+def get_coins(damage: int):
+    for i in range(1, 6):
+        if i * 20 <= damage < (i + 1) * 20:
+            return random.randrange(start=i*8, stop=(i + 1)*8, step=1)
+    return 50
+
+
 class SummonView(generic.DetailView):
 
     template_name = "summon.html"
 
     def get(self, request):
-        player = Player.objects.get(pk=1)
+        player = Player.objects.get(user=request.user)
 
         check_url = check_player_activity(player, ["summon", "index"])
         if check_url is not None:
             return redirect(check_url)
 
+        fee = 150 + Question.objects.filter(owner=request.user).count() * 20
         player.set_activity("summon4")
-        return render(request, "qa_rpg/summon.html", {"amount": range(4)})  # fixed 4 choices for now
+        return render(request, "qa_rpg/summon.html", {"amount": range(4), "fee": fee,
+                                                      "category": CATEGORY})  # fixed 4 choices for now
 
 
 def create(request):
     player = Player.objects.get(user=request.user)
 
     try:
-        question = Question.objects.create(question_text=request.POST['question'],
-                                           owner=request.user)
-        choices = []
+        question_text = request.POST['question']
+        choices = {}
         correct_index = int(request.POST['index'])
         for num in range(int(player.activity[6:])):
             if num == correct_index:
-                choices.append(Choice.objects.create(choice_text=request.POST[f'choice{num}'],
-                                                     correct_answer=True,
-                                                     question=question))
+                choices[request.POST[f'choice{num}']] = True
             else:
-                choices.append(Choice.objects.create(choice_text=request.POST[f'choice{num}'],
-                                                     correct_answer=False,
-                                                     question=question))
+                choices[request.POST[f'choice{num}']] = False
     except KeyError:
         messages.error(request, "Please fill in every field and select a correct answer.")
         return redirect("qa_rpg:summon")
 
+    summon_fee = int(request.POST['fee'])
+    if summon_fee > player.currency:
+        messages.error(request, "You don't have enough coins to summon a monster.")
+        return redirect("qa_rpg:summon")
+
+    question = Question.objects.create(question_text=question_text,
+                                       owner=request.user)
     question.save()
-    for choice in choices:
+    for choice_text in choices.keys():
+        choice = Choice.objects.create(choice_text=choice_text,
+                                       correct_answer=choices[choice_text],
+                                       question=question)
         choice.save()
     player.set_activity("index")
     messages.error(request, "Successfully summoned a new monster.")
+    player.currency -= summon_fee
+    player.save()
     return redirect('qa_rpg:index')
