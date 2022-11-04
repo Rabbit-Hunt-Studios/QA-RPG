0a675b163943b3f08bd836bce60c1f697c3900b2
RyukungG <ryu.nosuke@hotmail.com>
2022-11-04 22:19:24 +0700

    Add Log and Inventory to admin

--- qa_rpg/admin.py
+++ qa_rpg/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question, Choice, Player
+from .models import Question, Choice, Player, Log, Inventory
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -10,7 +10,7 @@ class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Question detail', {'fields': ['damage', 'currency',
-                                        'category', 'owner'],}),
+                                        'category', 'owner', 'enable'],}),
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'damage', 'currency',
@@ -33,5 +33,28 @@ class PlayerAdmin(admin.ModelAdmin):
     list_filter = ['max_hp', 'currency', 'activity']
 
 
+class LogAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Text Log Info', {'fields': ['log_text', 'player']}),
+        ('Question Log Info', {'fields': ['log_questions']})
+    ]
+    readonly_fields = ('player',)
+    list_display = ('player',)
+    list_filter = ['player']
+
+
+class InventoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Inventory Info', {'fields': ['player', 'player_inventory',
+                                       'dungeon_inventory', 'max_inventory']}),
+        ('Template Info', {'fields': ['question_template']})
+    ]
+    readonly_fields = ('player',)
+    list_display = ('player', 'max_inventory')
+    list_filter = ['player', 'max_inventory']
+
+
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Player, PlayerAdmin)
+admin.site.register(Log, LogAdmin)
+admin.site.register(Inventory, InventoryAdmin)
