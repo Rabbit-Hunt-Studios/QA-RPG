6050d30d34e6731bf97f8be232dc7070629fc55e
Mojizsz <isaraa.p@ku.th>
2022-11-12 20:16:00 +0700

    Adjust setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,7 +90,7 @@ SITE_ID = 2
 LOGIN_REDIRECT_URL = '/qa_rpg/index/'
 LOGOUT_REDIRECT_URL = '/qa_rpg/'
 SOCIALACCOUNT_LOGIN_ON_GET = True
-SOCIALACCOUNT_ADAPTER = "qa_rpg.adapter.CancelAccountAdapter"
+SOCIALACCOUNT_ADAPTER = "mysite.adapter.CancelAccountAdapter"
 
 ROOT_URLCONF = 'mysite.urls'
 
