8e6c9ebdb2292943f436199ca6f36042257f8711
RyukungG <ryu.nosuke@hotmail.com>
2022-11-14 23:19:14 +0700

    Update unittest

--- qa_rpg/test_views.py
+++ qa_rpg/test_views.py
@@ -924,7 +924,7 @@ class SelectItemTest(TestCase):
         self.player.set_activity("select_items")
         self.player.save()
         self.inventory = Inventory.objects.create(player=self.player)
-        self.inventory.update_inventory({0: 1, 1: 1, 6: 1}, "player")
+        self.inventory.update_inventory({0: 1, 1: 1, 6: 1, 7: 1}, "player")
         self.inventory.save()
 
     def test_select_item(self):
@@ -932,7 +932,7 @@ class SelectItemTest(TestCase):
         response = self.client.post(reverse("qa_rpg:select_items"), {"select": "01", "amount": 1})
         self.inventory = Inventory.objects.get(player=self.player)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(self.inventory.get_inventory("player")), 2)
+        self.assertEqual(len(self.inventory.get_inventory("player")), 3)
         self.assertEqual(len(self.inventory.get_inventory("dungeon")), 1)
 
     def test_select_item_max(self):
@@ -940,20 +940,21 @@ class SelectItemTest(TestCase):
         response = self.client.post(reverse("qa_rpg:select_items"), {"select": "01", "amount": 1})
         response = self.client.post(reverse("qa_rpg:select_items"), {"select": "11", "amount": 1})
         response = self.client.post(reverse("qa_rpg:select_items"), {"select": "61", "amount": 1})
+        response = self.client.post(reverse("qa_rpg:select_items"), {"select": "71", "amount": 1})
         self.inventory = Inventory.objects.get(player=self.player)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(len(self.inventory.get_inventory("player")), 1)
-        self.assertEqual(len(self.inventory.get_inventory("dungeon")), 2)
+        self.assertEqual(len(self.inventory.get_inventory("dungeon")), 3)
 
     def test_select_item_back(self):
         """Test player select item from dungeon inventory back to player inventory."""
         response = self.client.post(reverse("qa_rpg:select_items"), {"select": "01", "amount": 1})
         self.inventory = Inventory.objects.get(player=self.player)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(self.inventory.get_inventory("player")), 2)
+        self.assertEqual(len(self.inventory.get_inventory("player")), 3)
         self.assertEqual(len(self.inventory.get_inventory("dungeon")), 1)
         response = self.client.post(reverse("qa_rpg:select_items"), {"select": "02", "amount": 1})
         self.inventory = Inventory.objects.get(player=self.player)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(self.inventory.get_inventory("player")), 3)
+        self.assertEqual(len(self.inventory.get_inventory("player")), 4)
         self.assertEqual(len(self.inventory.get_inventory("dungeon")), 0)
