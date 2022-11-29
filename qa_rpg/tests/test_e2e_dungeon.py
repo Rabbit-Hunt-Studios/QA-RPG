01f348cb90196fa15be6c5adfe572392a3c66316
Mojizsz <isaraa.p@ku.th>
2022-11-29 13:34:30 +0700

    Add headless option and add assert in e2e_dungeon

--- qa_rpg/tests/test_e2e_dungeon.py
+++ qa_rpg/tests/test_e2e_dungeon.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-
-def test_enter_dungeon(self):
+def test_enter_dungeon():
     """Test e2e about the dungeon system."""
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
 
-    selenium = webdriver.Chrome(options=options)
+    ### Headless selenium
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    # selenium = webdriver.Chrome(options=chrome_options)
+
+    ### Default selenium
+    selenium = webdriver.Chrome()
     selenium.get("https://www.qarpg.tech/")
     selenium.find_element(By.XPATH, '//button[text()="Play Now!"]').click()
     selenium.find_element(By.XPATH, "//input[@name='login']").click()
@@ -17,16 +21,17 @@ def test_enter_dungeon(self):
     selenium.find_element(By.XPATH, '//button[text()="Login"]').click()
 
     selenium.find_element(By.XPATH, '//button[text()="dungeon"]').click()
+    assert ("/qa_rpg/select/" in selenium.current_url)
     selenium.find_element(By.XPATH, '/html/body/form/div[1]/div[1]/div/div[1]/label').click()
     selenium.find_element(By.XPATH, '/html/body/form/div[2]/div/div/input').click()
-    selenium.find_element(By.XPATH, '/html/body/form/div[2]/div/div/input').send_keys('3')
-    selenium.find_element(By.XPATH, '//button[text()="select"]').click()
+    selenium.find_element(By.XPATH, '//button[text()="Select"]').click()
     selenium.find_element(By.XPATH, '/html/body/form/div[1]/div[2]/div/div/label').click()
     selenium.find_element(By.XPATH, '/html/body/form/div[2]/div/div/input').click()
-    selenium.find_element(By.XPATH, '/html/body/form/div[2]/div/div/input').send_keys('1')
-    selenium.find_element(By.XPATH, '//button[text()="select"]').click()
+    selenium.find_element(By.XPATH, '//button[text()="Select"]').click()
     selenium.find_element(By.XPATH, '//button[text()="Dungeon"]').click()
 
-    selenium.find_element(By.XPATH, '//button[text()="Walk"]').click()
-    selenium.find_element(By.XPATH, '//button[text()="Walk"]').click()
+    assert ("/qa_rpg/dungeon/" in selenium.current_url)
     selenium.find_element(By.XPATH, '//button[text()="Exit"]').click()
+
+if __name__ == '__main__':
+    test_enter_dungeon()
