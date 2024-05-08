444af8befba72624afaa74719ebbeebcf4e2124a
RyukungG <ryu.nosuke@hotmail.com>
2024-05-09 01:21:52 +0700

    Update database encryption and add Google Recaptcha to signin page

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
+
 
 
 def signup(request):
@@ -10,16 +12,41 @@ def signup(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            username = form.cleaned_data.get('username')
-            raw_passwd = form.cleaned_data.get('password')
         else:
             messages.error(request, "Your form is invalid! please fill out again.")
             return redirect('signup')
         return redirect('login')
     else:
         form = RegisterForm()
-    return render(request, 'account/signup.html', {'form': form})
+    return render(request=request,
+                  template_name='account/signup.html',
+                  context={'form': form})
 
+def custom_login(request):
+    """Log in."""
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data.get('username'),
+                raw_passwd=form.cleaned_data.get('password'),
+            )
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Welcome {user.username}")
+        else:
+            messages.error(request, "Your form is invalid! please fill out again.")
+            return redirect('login')
+    form = LoginForm()
+    return render(request=request,
+                  template_name='account/login.html',
+                  context={'form': form})
+
+@login_required
+def custom_logout(request):
+    logout(request)
+    messages.info(request, "Logged out successfully")
+    return redirect('qa_rpg/index/')
 
 def policy(request):
     print(request)
